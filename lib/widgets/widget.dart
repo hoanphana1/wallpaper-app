14a56bf23c09b92c31faf58f9c228d97f5db1a0d
DESKTOP-5U1VBJ6\Admin <hoanphana1@gmail.com>
2021-03-02 11:36:34 +0700

    storage error

---
+++ lib/widgets/widget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaperapp/model/wallpaper_model.dart';
+import 'package:wallpaperapp/views/image_view.dart';
+
+Widget brandName() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        "Wallpaper",
+        style: TextStyle(color: Colors.black87),
+      ),
+      Text(
+        "Hub",
+        style: TextStyle(color: Colors.lightGreen),
+      )
+    ],
+  );
+}
+
+Widget wallpapersList({List<WallpaperModel> wallpapers, context}) {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    child: GridView.count(
+      shrinkWrap: true,
+      physics: ClampingScrollPhysics(),
+      crossAxisCount: 2,
+      childAspectRatio: 0.6,
+      mainAxisSpacing: 6.0,
+      crossAxisSpacing: 6.0,
+      children: wallpapers.map((wallpaper) {
+        return GridTile(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ImageView(
+                            imgUrl: wallpaper.src.portrait,
+                          )));
+            },
+            child: Hero(
+              tag: wallpaper.src.portrait,
+              child: Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.network(
+                    wallpaper.src.portrait,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      }).toList(),
+    ),
+  );
+}
